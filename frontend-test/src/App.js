e46a465e781ad3fa00b63948b3e887380a249b7a
Affan Khan <affanu02@gmail.com>
2024-04-08 00:20:56 -0400

    completed css styling, next step add all info

--- frontend-test/src/App.js
+++ frontend-test/src/App.js
@@ -7,7 +7,49 @@ function App() {
         <h1>Coffee Tables</h1>
         <div className="section-products">
           <div className="product">
-            <img src="./assets/coffee-table.webp" alt="Coffee Table" />
+            <img
+              className="product-img"
+              src="./assets/coffee-table.webp"
+              alt="Coffee Table"
+            />
+            <div className="button-container">
+              <button className="material-button">
+                Walnut <div className="walnut"></div>
+              </button>
+            </div>
+            <div className="product-name">Mensa Coffee Table</div>
+            <div className="price">
+              $360 or financing |{" "}
+              <a href="/customize" className="customize">
+                Customize{" "}
+              </a>
+            </div>
+          </div>
+          <div className="product">
+            <img
+              className="product-img"
+              src="./assets/coffee-table.webp"
+              alt="Coffee Table"
+            />
+            <div className="button-container">
+              <button className="material-button">
+                Walnut <div className="walnut"></div>
+              </button>
+            </div>
+            <div className="product-name">Mensa Coffee Table</div>
+            <div className="price">
+              $360 or financing |{" "}
+              <a href="/customize" className="customize">
+                Customize{" "}
+              </a>
+            </div>
+          </div>
+          <div className="product">
+            <img
+              className="product-img"
+              src="./assets/coffee-table.webp"
+              alt="Coffee Table"
+            />
             <div className="button-container">
               <button className="material-button">
                 Walnut <div className="walnut"></div>
