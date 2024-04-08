a225074011e0766c17f47f6e66f0d2a4c0050fd0
Affan Khan <affanu02@gmail.com>
2024-04-07 23:29:13 -0400

    Added in basic CSS for products-card

--- frontend-test/src/App.js
+++ frontend-test/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="section">
+        <h1>Coffee Tables</h1>
+        <div className="section-products">
+          <div className="product">
+            <img src="./assets/coffee-table.webp" alt="Coffee Table" />
+            <button className="material-button walnut">Walnut</button>
+            <div className="product-name">Mensa Coffee Table</div>
+            <div className="price">
+              $360 or financing |{" "}
+              <a href="/customize" className="customize">
+                Customize{" "}
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
